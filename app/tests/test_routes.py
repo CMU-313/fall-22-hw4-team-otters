5786751fc1ac6c85fda441bbd954551bd0253986
Grace Xin <qingqinggrace@gmail.com>
2022-10-29 17:07:50 -0400

    Add predict route test for invalid requests

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -13,3 +13,17 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+# incorrect request body => error status code, error message
+def test_predict_route_invalid_request():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+
+    response = client.get(url)
+
+    print(response.get_data())
+    assert response.status_code == 400
+
+# correct request body
