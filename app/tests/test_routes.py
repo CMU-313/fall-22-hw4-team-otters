493419227d1daa2a259411bed87230d42dcb60a9
Grace Xin <qingqinggrace@gmail.com>
2022-11-01 11:13:34 -0400

    Add tests for /applications and /predict endpoints

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -13,3 +13,54 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_post_applicants_route_invalid_format():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/applicants'
+
+    response = client.post(url)
+    assert response.status_code == 400
+
+def test_get_applicants_route_invalid_request():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/applicants'
+
+    response = client.get(url)
+    assert response.status_code == 400
+
+def test_get_applicants_route():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/applicants'
+
+    response = client.post(url)
+    assert response.status_code == 200
+
+    response = client.get(url)
+    assert response.status_code == 400
+
+def test_predict_route_invalid_request():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+
+    response = client.get(url)
+
+    assert response.status_code == 400
+    
+def test_predict_route():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.get_data() == 1 or response.get_data() == 0
