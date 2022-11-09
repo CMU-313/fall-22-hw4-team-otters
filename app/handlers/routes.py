0dfefa4bb1b6af226c0ed091ceb4d93cd5833ea9
Grace Xin <gracexin2003@users.noreply.github.com>
2022-11-08 21:58:14 -0500

    Update endpoints and tests based on new model (#8)
    * Update endpoints and tests based on new model
    
    * Fix endpoints and tests (1)
    
    * Update predict endpoint and tests
    
    * Fix and integrate all endpoints and tests
    
    Co-authored-by: Laura Wu <75864875+lywu-andrew@users.noreply.github.com>

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -16,9 +16,10 @@ def configure_routes(app):
         return "try the predict route it is great!"
     
     attributes = [
-        { 'age': 0, 'absences': 0, 'health': 0 }
+        { 'health': 0, 'absences': 0, 'age': 0, 'failures': 0, 'Dalc': 0, 'internet_int': 0, 
+          'higher_int': 0, 'paid_int': 0, 'studytime': 0, 'address_int': 0 }
     ]
-    
+
     @app.route('/applicants', methods=['GET', 'POST'])
     def applicants():
         if request.method == "GET":
@@ -26,23 +27,42 @@ def configure_routes(app):
         if request.method == "POST":
             result = {}
             applicant = request.get_json()
-            if 'age' in applicant and 'absences' in applicant and 'health' in applicant:
-                result['age'] = applicant['age']
-                result['absences'] = applicant['absences']
+            if ('health' in applicant and 'absences' in applicant and 'age' in applicant 
+            and 'failures' in applicant and 'Dalc' in applicant and 'internet_int' in applicant 
+            and 'higher_int' in applicant and 'paid_int' in applicant and 'studytime' in applicant
+            and 'address_int' in applicant):
                 result['health'] = applicant['health']
+                result['absences'] = applicant['absences']
+                result['age'] = applicant['age']
+                result['failures'] = applicant['failures']
+                result['Dalc'] = applicant['Dalc']
+                result['internet_int'] = applicant['internet_int']
+                result['higher_int'] = applicant['higher_int']
+                result['paid_int'] = applicant['paid_int']
+                result['studytime'] = applicant['studytime']
+                result['address_int'] = applicant['address_int']
                 attributes.append(result)
-                return "Successfully added new applicant attributes: " + jsonify(attributes[-1]), 200
+                return jsonify(attributes[-1]), 200
             else:
-                return "Invalid input: dictionary should contain 'age', 'absences', and 'health' attributes", 400
+                return "Invalid input: dictionary should contain 'health', 'absences', 'age', 'failures', \
+                    'Dalc', 'internet_int', 'higher_int', 'paid_int', 'studytime', and 'address_int' attributes.", 400
 
 
-    @app.route('/predict', methods=['GET']
+    @app.route('/predict', methods=['GET'])
     def predict():
+        #use entries from the query string here but could also use json
         query_df = pd.DataFrame({
-            'age': pd.Series(attributes[-1]['age']),
             'health': pd.Series(attributes[-1]['health']),
-            'absences': pd.Series(attributes[-1]['absences'])
+            'absences': pd.Series(attributes[-1]['absences']),
+            'age': pd.Series(attributes[-1]['age']),
+            'failures': pd.Series(attributes[-1]['failures']),
+            'Dalc': pd.Series(attributes[-1]['Dalc']),
+            'internet_int': pd.Series(attributes[-1]['internet_int']),
+            'higher_int': pd.Series(attributes[-1]['higher_int']),
+            'paid_int': pd.Series(attributes[-1]['paid_int']),
+            'studytime': pd.Series(attributes[-1]['studytime']),
+            'address_int': pd.Series(attributes[-1]['address_int'])
         })
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
-        return jsonify(np.ndarray.item(prediction))
+        return jsonify(np.ndarray.item(prediction)), 200
